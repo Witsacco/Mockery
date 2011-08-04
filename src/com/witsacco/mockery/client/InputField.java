e0f673af233f150862941273d81b91940fbfc07c
Ryan Witko <witko.ryan@gmail.com>
2011-08-04 07:08:39 -0400

    Returning default values for compilation purposes.

--- src/com/witsacco/mockery/client/InputField.java
+++ src/com/witsacco/mockery/client/InputField.java
@@ -9,10 +9,12 @@ public class InputField {
 	
 	public String getText() {
 		//TODO Grab text from DOM
+		return "";
 	}
 	
 	private boolean isValid( String text ) {
 		//TODO implement input validation logic (FieldVerifier?)
+		return true;
 	}
 	
 }
