834a79c54df36bfaa1777a70e2e7c78e9ca5e577
rywit <witko.ryan@gmail.com>
2011-09-01 19:00:59 -0400

    Revert "test commit 2"
    This reverts commit 583f84c0b3dd4a6af01d50a7be7affb64808580f.

--- src/com/witsacco/mockery/client/Mockery.java
+++ src/com/witsacco/mockery/client/Mockery.java
@@ -45,8 +45,6 @@ public class Mockery implements EntryPoint {
 	// The room that the user is in
 	private int activeRoom;
     
-	private int x;
-	
 	// Our update poller
 	private UpdatePoller poller;
 
