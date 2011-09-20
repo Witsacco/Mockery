d3eb125eefcda4163d5077df90430aba15a083d4
rywit <witko.ryan@gmail.com>
2011-09-19 20:36:22 -0400

    Now display reasons for the scores

--- src/com/witsacco/mockery/server/MessageJudge.java
+++ src/com/witsacco/mockery/server/MessageJudge.java
@@ -9,6 +9,7 @@ import org.xeustechnologies.googleapi.spelling.SpellChecker;
 import org.xeustechnologies.googleapi.spelling.SpellCorrection;
 import org.xeustechnologies.googleapi.spelling.SpellResponse;
 
+import com.google.gwt.dev.util.Pair;
 import com.witsacco.mockery.client.MessageScore;
 
 public class MessageJudge {
@@ -22,14 +23,14 @@ public class MessageJudge {
 	private static final int RANDOM_LIMIT = 3;
 
 	private static Random rand = new Random();
-	
-	private HashMap<String, Integer> wordValues;
+
+	private HashMap< String, WordScore > wordValues;
 
 	// Empty constructor
 	private MessageJudge() {
-		
+
 		initWordValues();
-		
+
 	}
 
 	// Get single instance of MessageJudge
@@ -50,8 +51,11 @@ public class MessageJudge {
 		}
 
 		// Compare to our set of words with pre-defined scores
-		score += scorePhrase( messageBody );
-		
+		for (WordScore wordScore : scorePhrase( messageBody ) ) {
+			score += wordScore.getScore();
+			reasons.add( wordScore.getReason() );
+		}
+
 		return new MessageScore( score, reasons );
 	}
 
@@ -82,41 +86,66 @@ public class MessageJudge {
 	private int getRandomFluctuation() {
 		return rand.nextInt( RANDOM_LIMIT * 2 ) - RANDOM_LIMIT + 1;
 	}
-	
-	private int scorePhrase( String phrase ) {
-		int sumScore = 0;
-		
-		StringTokenizer st = new StringTokenizer( phrase);
 
-		while (st.hasMoreTokens()) {
-			sumScore += scoreWord( st.nextToken() );
+	private ArrayList< WordScore > scorePhrase( String phrase ) {
+		ArrayList< WordScore > scores = new ArrayList< WordScore >();
+
+		StringTokenizer st = new StringTokenizer( phrase );
+
+		while ( st.hasMoreTokens() ) {
+			WordScore score = scoreWord( st.nextToken() );
+
+			if ( score != null ) {
+				scores.add( score );
+			}
 		}
-		
-		//TODO: remove words that were present in the scoring map from spelling consideration
-		
-		return sumScore;
+
+		// TODO: remove words that were present in the scoring map from spelling
+		// consideration
+
+		return scores;
 	}
-	
-	private int scoreWord( String word ) {
-		
-		Integer score = wordValues.get( word.toLowerCase() ); 
-		
+
+	private WordScore scoreWord( String word ) {
+
+		WordScore score = wordValues.get( word.toLowerCase() );
+
 		if ( score != null ) {
-			return score; 
+			return score;
 		}
-		
-		return 0;
+
+		return null;
 	}
-	
+
 	private void initWordValues() {
-		wordValues = new HashMap< String, Integer >();
-		
-		// Keys should be lowercase.  When scoring, incoming words will be converted to lowercase
-		wordValues.put( "nyu", 5 );
-		wordValues.put( "nell", -10 );
-		wordValues.put( "mets", -5 );
-		wordValues.put( "molitor", 3 );
-		wordValues.put( "lol", -5 );
-		wordValues.put( ":)", -15 );
+		wordValues = new HashMap< String, WordScore >();
+
+		// Keys should be lowercase. When scoring, incoming words will be
+		// converted to lowercase
+		wordValues.put( "nyu", new WordScore( 5, "Violet Pride!!!" ) );
+		wordValues.put( "nell", new WordScore( -10, "Don't mention the ex" ) );
+		wordValues.put( "mets", new WordScore( -5, "Only 15 games back..." ) );
+		wordValues.put( "molitor", new WordScore( 3, "Don't mess with Molly" ) );
+		wordValues.put( "lol", new WordScore( -5, "Lawl" ) );
+		wordValues.put( ":)", new WordScore( -15, "More like :(" ) );
+	}
+
+	private class WordScore {
+
+		private int score;
+		private String reason;
+
+		public WordScore( int score, String reason ) {
+			this.score = score;
+			this.reason = reason;
+		}
+
+		public int getScore() {
+			return score;
+		}
+
+		public String getReason() {
+			return reason;
+		}
 	}
 }
